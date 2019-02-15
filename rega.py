365b4871eb2ceb9b320c401382678b98b08ff2ba
Johan Viklund <johan.viklund@nbis.se>
2019-02-15 16:25:40 +0100

    Applied flake8 code linter
    Co-authored-by: jonandernovella <novella.uu@gmail.com>

--- rega.py
+++ rega.py
@@ -8,39 +8,41 @@ from distutils import dir_util
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
-from os import walk
-from pkg_resources import iter_entry_points
-from click_plugins import with_plugins
-from distutils import dir_util
 
 logging.basicConfig(level=logging.INFO)
 
 DEFAULT_IMAGE = 'novella/rega:latest'
 
+
 @click.group()
 def main():
     """REGA is a tool for provisioning RKE clusters."""
 
+
 @main.command('init')
 @click.argument('dir')
-@click.option('-I','--image', default=DEFAULT_IMAGE, envvar='REGA_PROVISIONER_IMG')
-def init(dir,image):
+@click.option('-I', '--image', default=DEFAULT_IMAGE,
+              envvar='REGA_PROVISIONER_IMG')
+def init(dir, image):
     logging.info("""Initilaizing a new environment in {}""".format(dir))
     client = docker.from_env()
     client.images.pull(image)
     check_init_dir()
     create_deployment(dir)
 
+
 @main.command('version')
-@click.option('-I','--image', default=DEFAULT_IMAGE, envvar='REGA_PROVISIONER_IMG')
+@click.option('-I', '--image', default=DEFAULT_IMAGE,
+              envvar='REGA_PROVISIONER_IMG')
 def version(image):
     logging.info("""REGA provisioner version is {}""".format(image))
 
+
 @main.command('apply')
-@click.option('-I','--image', default=DEFAULT_IMAGE, envvar='REGA_PROVISIONER_IMG',\
-help='Applies the Terraform plan to spawn the desired resources')
+@click.option('-I', '--image', default=DEFAULT_IMAGE,
+              envvar='REGA_PROVISIONER_IMG',
+              help='Applies the Terraform plan to spawn the desired resources')
 def apply(image):
-    #client.images.pull(image)
     logging.info("""Applying setup""")
     check_environment()
 
@@ -49,21 +51,22 @@ def apply(image):
 
 
 @main.command('destroy')
-@click.option('-I','--image', default=DEFAULT_IMAGE, envvar='REGA_PROVISIONER_IMG',\
-help='Releses all resources available in the Terraform state')
+@click.option('-I', '--image', default=DEFAULT_IMAGE,
+              envvar='REGA_PROVISIONER_IMG',
+              help='Releses all resources available in the Terraform state')
 def destroy(image):
-    #client.images.pull(image)
     logging.info("""Destroying the infrastructure""")
     check_environment()
 
     run_in_container(['terraform destroy -force'], image)
 
+
 @main.command('terraform')
 @click.argument('extra_args')
-@click.option('-I','--image', default=DEFAULT_IMAGE, envvar='REGA_PROVISIONER_IMG',\
-help='Executes the terraform command in the provisioner container with the provided args')
-def terraform(extra_args,image):
-    #client.images.pull(image)
+@click.option('-I', '--image', default=DEFAULT_IMAGE,
+              envvar='REGA_PROVISIONER_IMG',
+              help='Executes the terraform command in the provisioner container with the provided args')
+def terraform(extra_args, image):
     logging.info("""Running terraform with arguments: {}""".format(extra_args))
     check_environment()
 
@@ -72,42 +75,44 @@ def terraform(extra_args,image):
 
 @main.command('openstack')
 @click.argument('extra_args')
-@click.option('-I','--image', default=DEFAULT_IMAGE, envvar='REGA_PROVISIONER_IMG',\
-help='Executes the openstack command in the provisioner container with the provided args')
-def openstack(extra_args,image):
-    #client.images.pull(image)
+@click.option('-I', '--image', default=DEFAULT_IMAGE,
+              envvar='REGA_PROVISIONER_IMG',
+              help='Executes the openstack command in the provisioner container with the provided args')
+def openstack(extra_args, image):
     logging.info("""Running openstack with arguments: {}""".format(extra_args))
 
     run_in_container(['openstack {}'.format(extra_args)], image)
 
 
-def run_in_container(commands, image, detach=True):
+def run_in_container(commands, image):
     client = docker.from_env()
-    env    = list(filter_vars(os.environ))
+    env = list(filter_vars(os.environ))
     volume_mount = {os.getcwd(): {'bind': '/mnt/deployment/', 'mode': 'rw'}}
     container_wd = '/mnt/deployment/'
 
-    assert type(commands) == type(list()), "First argument should be a list"
+    assert isinstance(commands, list()), "First argument should be a list"
 
     commands_as_string = " && ".join(commands)
     runner = client.containers.run(
-            image,
-            volumes     = volume_mount,
-            environment = env,
-            working_dir = container_wd,
-            entrypoint  = ['bash', '-c'],
-            command     = f'"{commands}"',
-            detach      = detach
+        image,
+        volumes=volume_mount,
+        environment=env,
+        working_dir=container_wd,
+        entrypoint=['bash', '-c'],
+        command=f'"{commands_as_string}"',
+        detach=True
     )
 
     for line in runner:
         print(line.decode())
 
+
 def create_deployment(dir):
     """copy relevant files to new folder"""
     if os.path.exists('deployment-template'):
         dir_util.mkpath(dir)
-        subprocess.call('cp -r deployment-template/* ./{}/'.format(dir), shell=True)
+        subprocess.call('cp -r deployment-template/* ./{}/'.format(dir),
+                        shell=True)
     else:
         sys.stderr.write("Error: deployment-template folder not found. Are you in the right directory?\n")
         sys.exit(1)
@@ -118,6 +123,7 @@ def create_deployment(dir):
             key.write(pu)
         with open(dir + '/ssh_key', 'w') as key:
             key.write(pv)
+
 
 def check_environment():
     if not os.environ.get('OS_AUTH_URL', False):
@@ -132,10 +138,12 @@ def check_environment():
         sys.stderr.write("Error: terraform.tfvars not found. Are you in the right directory?\n")
         sys.exit(1)
 
+
 def check_init_dir():
     if not os.path.exists('deployment-template'):
         sys.stderr.write("Error: deployment-template folder not found. Are you in the right directory?\n")
         sys.exit(1)
+
 
 def create_key_pair():
     key = rsa.generate_private_key(
@@ -153,7 +161,10 @@ def create_key_pair():
     )
     return (public_key.decode('utf-8'), private_key.decode('utf-8'))
 
+
 def filter_vars(seq):
-   for key, val in seq.items():
-       if key.startswith('TF_'): yield key+'='+val
-       elif key.startswith('OS_'): yield key+'='+val
+    for key, val in seq.items():
+        if key.startswith('TF_'):
+            yield key + '=' + val
+        elif key.startswith('OS_'):
+            yield key + '=' + val
