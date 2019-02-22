a748d0bf5bcd13dfb5fa986eb11eb9f75cdaf2ee
jonandernovella <novella.uu@gmail.com>
2019-02-22 11:13:02 +0100

    Added output for access ips

--- deployment-template/modules/node/outputs.tf
+++ deployment-template/modules/node/outputs.tf
@@ -8,6 +8,11 @@ output "public_ip_list" {
   value       = ["${openstack_compute_floatingip_v2.floating_ip.*.address}"]
 }
 
+output "access_ip_list" {
+  description = "IP addresses for Ansible inventory"
+  value = "${local.address_list}"
+}
+
 output "private_ip_list" {
   description = "List of local IP addresses"
   value = ["${openstack_compute_instance_v2.instance.*.network.0.fixed_ip_v4}"]
