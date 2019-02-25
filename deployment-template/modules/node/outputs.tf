c484251702e9608a7cf9acb7e7d8cfe960ef80df
jonandernovella <novella.uu@gmail.com>
2019-02-25 15:36:52 +0100

    Added hostnames ouput

--- deployment-template/modules/node/outputs.tf
+++ deployment-template/modules/node/outputs.tf
@@ -27,3 +27,7 @@ output "associate_floating_ip_id_list" {
   description = "Associate floating IP resource ID list"
   value       = ["${openstack_compute_floatingip_associate_v2.associate_floating_ip.*.id}"]
 }
+
+output "hostnames" {
+  value = ["${openstack_compute_instance_v2.instance.*.name}"]
+}
