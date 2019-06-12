12bcda90093c2bcd9f1f4a5f50ff3c25c5fb912b
jonandernovella <novella.uu@gmail.com>
2019-06-12 11:11:30 +0200

    Bumped rega package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rega',
-    version='0.4.3',
+    version='0.4.4',
     packages=find_packages(),
     py_modules=['rega'],
     include_package_data=True,
