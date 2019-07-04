4cc005798c80cf44b9ffe6a29dd9f3f63015e866
jonandernovella <novella.uu@gmail.com>
2019-07-04 22:29:46 +0200

    Refactor security group module syntax

--- deployment-template/modules/secgroup/variables.tf
+++ deployment-template/modules/secgroup/variables.tf
@@ -1,15 +1,16 @@
-variable name_prefix {
+variable "name_prefix" {
   description = "Prefix for the security group name"
 }
 
-variable allowed_ingress_tcp {
-  type        = "list"
+variable "allowed_ingress_tcp" {
+  type        = list(string)
   description = "Allowed TCP ingress traffic"
   default     = []
 }
 
-variable allowed_ingress_udp {
-  type        = "list"
+variable "allowed_ingress_udp" {
+  type        = list(string)
   description = "Allowed UDP ingress traffic"
   default     = []
 }
+
