cb3f41da9f9a34e33e6496cbbeaf725b50395526
jonandernovella <novella.uu@gmail.com>
2019-07-04 22:30:07 +0200

    Refactor rke module syntax

--- deployment-template/modules/rke/main.tf
+++ deployment-template/modules/rke/main.tf
@@ -1,34 +1,39 @@
 # Provision RKE
-resource rke_cluster "cluster" {
+resource "rke_cluster" "cluster" {
   cloud_provider {
-      name = "openstack"
-      openstack_cloud_config = {
-        global = {
-          username = "${var.os_username}"
-          password = "${var.os_password}"
-          auth_url = "${var.os_auth_url}"
-          tenant_id = "${var.os_project_id}"
-          tenant_name = "${var.os_project_name}"
-          domain_name = "${var.os_user_domain_name}"
-        }
-        block_storage = {
-          bs_version = "auto"
-          ignore_volume_az = "true"
-          trust_device_path = "false"
-        }
+    name = "openstack"
+    openstack_cloud_config {
+      global {
+        username    = var.os_username
+        password    = var.os_password
+        auth_url    = var.os_auth_url
+        tenant_id   = var.os_project_id
+        tenant_name = var.os_project_name
+        domain_name = var.os_user_domain_name
+      }
+      block_storage {
+        bs_version        = "auto"
+        ignore_volume_az  = "true"
+        trust_device_path = "false"
       }
     }
+  }
 
-  nodes_conf = ["${var.node_mappings}"]
+  authentication {
+    strategy = "x509"
+    sans     = flatten([var.kubeapi_sans_list])
+  }
 
-  bastion_host = {
-    address      = "${var.ssh_bastion_host}"
-    user         = "${var.ssh_user}"
-    ssh_key_path = "${var.ssh_key}"
+  nodes_conf = flatten([var.node_mappings])
+
+  bastion_host {
+    address      = var.ssh_bastion_host
+    user         = var.ssh_user
+    ssh_key_path = var.ssh_key
     port         = 22
   }
 
-  ingress = {
+  ingress {
     provider = "nginx"
 
     node_selector = {
@@ -36,17 +41,12 @@ resource rke_cluster "cluster" {
     }
   }
 
-  authentication = {
-    strategy = "x509"
-    sans     = ["${var.kubeapi_sans_list}"]
-  }
-
-  ignore_docker_version = "${var.ignore_docker_version}"
-  kubernetes_version = "${var.kubernetes_version}"
+  ignore_docker_version = var.ignore_docker_version
+  kubernetes_version    = var.kubernetes_version
 
   # Workaround: make sure resources are created and deleted in the right order
   provisioner "local-exec" {
-    command = "# ${join(",",var.rke_cluster_deps)}"
+    command = "# ${join(",", var.rke_cluster_deps)}"
   }
 
   addons = <<EOL
@@ -67,57 +67,61 @@ EOL
 
 # Write YAML configs
 locals {
-  api_access       = "https://${element(var.kubeapi_sans_list,0)}:6443"
+  api_access = "https://${element(var.kubeapi_sans_list, 0)}:6443"
   api_access_regex = "/https://\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}:6443/"
 }
 
-resource local_file "kube_config_cluster" {
-  count    = "${var.write_kube_config_cluster ? 1 : 0}"
+resource "local_file" "kube_config_cluster" {
+  count = var.write_kube_config_cluster ? 1 : 0
   filename = "${path.root}/kube_config_cluster.yml"
 
   # Workaround: https://github.com/rancher/rke/issues/705
-  content = "${replace(rke_cluster.cluster.kube_config_yaml, local.api_access_regex, local.api_access)}"
+  content = replace(
+    rke_cluster.cluster.kube_config_yaml,
+    local.api_access_regex,
+    local.api_access,
+  )
 }
 
 resource "local_file" "cluster_yml" {
-  count    = "${var.write_cluster_yaml ? 1 : 0}"
+  count = var.write_cluster_yaml ? 1 : 0
   filename = "${path.root}/cluster.yml"
-  content  = "${rke_cluster.cluster.rke_cluster_yaml}"
+  content = rke_cluster.cluster.rke_cluster_yaml
 }
 
 # Configure Kubernetes provider
 provider "kubernetes" {
-  host                   = "${local.api_access}"
-  username               = "${rke_cluster.cluster.kube_admin_user}"
-  client_certificate     = "${rke_cluster.cluster.client_cert}"
-  client_key             = "${rke_cluster.cluster.client_key}"
-  cluster_ca_certificate = "${rke_cluster.cluster.ca_crt}"
+  host = local.api_access
+  username = rke_cluster.cluster.kube_admin_user
+  client_certificate = rke_cluster.cluster.client_cert
+  client_key = rke_cluster.cluster.client_key
+  cluster_ca_certificate = rke_cluster.cluster.ca_crt
 }
 
 resource "kubernetes_service_account" "tiller" {
   metadata {
-    name      = "terraform-tiller"
+    name = "terraform-tiller"
     namespace = "kube-system"
   }
 }
 
 resource "kubernetes_cluster_role_binding" "tiller" {
-  depends_on = ["kubernetes_service_account.tiller"]
+  depends_on = [kubernetes_service_account.tiller]
   metadata {
     name = "terraform-tiller"
   }
 
   role_ref {
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
+    kind = "ClusterRole"
+    name = "cluster-admin"
     api_group = "rbac.authorization.k8s.io"
   }
 
   subject {
     kind = "ServiceAccount"
     name = "terraform-tiller"
-
     api_group = ""
     namespace = "kube-system"
   }
 }
+
--- deployment-template/modules/rke/outputs.tf
+++ deployment-template/modules/rke/outputs.tf
@@ -1,11 +1,17 @@
-output kube_config_cluster {
+output "kube_config_cluster" {
   description = "Kubeconfig file"
+
   # Workaround: https://github.com/rancher/rke/issues/705
-  value     = "${replace(rke_cluster.cluster.kube_config_yaml, local.api_access_regex, local.api_access)}"
+  value = replace(
+    rke_cluster.cluster.kube_config_yaml,
+    local.api_access_regex,
+    local.api_access,
+  )
   sensitive = true
 }
 
-output cluster_yml {
+output "cluster_yml" {
   description = "RKE cluster.yml file"
-  value       = "${rke_cluster.cluster.rke_cluster_yaml}"
+  value       = rke_cluster.cluster.rke_cluster_yaml
 }
+
