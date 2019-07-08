00be0a37ab55352cfbab1124e49a687ac9c70e25
jonandernovella <novella.uu@gmail.com>
2019-07-08 09:51:23 +0200

    Remove node_mappings output

--- deployment-template/modules/node/outputs.tf
+++ deployment-template/modules/node/outputs.tf
@@ -1,8 +1,3 @@
-output "node_mappings" {
-  description = "RKE node mappings"
-  value       = flatten([data.rke_node_parameter.node_mappings.*.json])
-}
-
 output "public_ip_list" {
   description = "List of floating IP addresses"
   value       = flatten([openstack_compute_floatingip_v2.floating_ip.*.address])
