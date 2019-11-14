2288b466a6ce90858aa636e89f924e693b2544ce
Jon Ander Novella <jonandernovella@users.noreply.github.com>
2019-11-14 10:26:44 +0100

    Bump rega package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rega',
-    version='1.8.0',
+    version='1.9.0',
     packages=['rega'],
     description='REGA CLI for provisioning RKE clusters',
     author='Jon Ander Novella and Johan Viklund',
