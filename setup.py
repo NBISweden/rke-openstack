356dc9d91ffc79c081b1878644bb707f0afd404c
jonandernovella <novella.uu@gmail.com>
2020-05-11 11:56:34 +0200

    Simplify installation
    A requirements file is now provided to install dependencies. Makefile
    was removed as it is not in use.

--- setup.py
+++ setup.py
@@ -2,23 +2,10 @@ from setuptools import setup
 
 setup(
     name='rega',
-    version='1.9.8',
+    version='2.0.0',
     packages=['rega'],
     description='REGA CLI for provisioning RKE clusters',
     author='Jon Ander Novella and Johan Viklund',
-    install_requires=[
-        'click==7.0',
-        'pyhcl==0.3.12',
-        'click-plugins==1.1.1',
-        'docker==4.0.1',
-        'cryptography==2.7',
-        'PyYAML==5.1.1',
-        'prettytable==0.7.2'
-    ],
-    setup_requires=[
-        'flake8==3.7.7',
-        'tox>=3.14.0',
-    ],
     entry_points='''
         [console_scripts]
         rega=rega.cmd:main
